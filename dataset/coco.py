02e96f4bfd7b88d5335e686321513590a2a64bf9
josh <josh3255@ziovsion.co.kr>
2023-04-21 11:00:09 +0900

    Modify coco.py

--- dataset/coco.py
+++ dataset/coco.py
@@ -24,7 +24,7 @@ class COCODataset(Dataset):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (self.img_size, self.img_size))
         img = img / 255.0
-        
+
         img = torch.tensor(img, dtype=torch.float32)
         img = img.permute(2, 0, 1)
         
@@ -74,21 +74,6 @@ class COCODataset(Dataset):
             target[cell_pos_x][cell_pos_y][0:5] = torch.tensor([cx, cy, w, h, 1])
             target[cell_pos_x][cell_pos_y][5:10] = torch.tensor([cx, cy, w, h, 1])
             target[cell_pos_x][cell_pos_y][10+int(cls)] = 1
+            target[cell_pos_x][cell_pos_y][5 * self.args.B + int(cls)] = 1
 
         return target
-
-def collate_fn(batch):
-    max_objs = max([len(b[1]) for b in batch])
-    images = []
-    targets = []
-
-    for image, target in batch:
-        images.append(image)
-
-        num_objs = len(target)
-        padding = torch.zeros((max_objs - num_objs, 5))
-        target = torch.tensor(target)
-        target = torch.cat((target, padding), dim=0)
-        targets.append(target)
-
-    return torch.stack(images), torch.stack(targets)
