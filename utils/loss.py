b1257ef21304226d8732a8a1d3e80abac87a036a
josh <josh3255@ziovsion.co.kr>
2023-04-21 18:22:59 +0900

    add noobj loss and iou term

--- utils/loss.py
+++ utils/loss.py
@@ -17,9 +17,9 @@ class YOLOLoss(nn.Module):
         
     def forward(self, pred, target):
         batch_size = pred.shape[0]
-        
+
         mse_loss = torch.nn.MSELoss()
-        bce_loss = torch.nn.BCELoss()
+        ce_loss = torch.nn.CrossEntropyLoss()
 
         # localization loss
         obj_mask = target[:, :, :, 4] > 0
@@ -32,47 +32,61 @@ class YOLOLoss(nn.Module):
         target_bbox1 = obj_target[:, 0:4]
         target_bbox2 = obj_target[:, 5:9]
 
-        ious1 = calc_iou(pred_bbox1, target_bbox1)
-        ious2 = calc_iou(pred_bbox2, target_bbox2)
+        ious1 = compute_iou(pred_bbox1, target_bbox1)
+        ious2 = compute_iou(pred_bbox2, target_bbox2)
 
+        # cx, cy to tx, ty after compute iou
         int_tb1 = torch.floor(target_bbox1[:, :2])
         target_bbox1[:, :2] = target_bbox1[:, :2] - int_tb1
         int_tb2 = torch.floor(target_bbox2[:, :2])
         target_bbox2[:, :2] = target_bbox2[:, :2] - int_tb2
-
-        # objectness loss
+        
+        # object loss
         pred_obj1 = obj_pred[:, 4]
         pred_obj2 = obj_pred[:, 9]
-        pred_class = obj_pred[:, 10:13]
+        pred_class = obj_pred[:, 5 * self.B : 5 * self.B + self.C]
 
         target_obj1 = obj_target[:, 4]
         target_obj2 = obj_target[:, 9]
-        target_class = obj_target[:, 10:13]
+        target_class = obj_target[:, 5 * self.B : 5 * self.B + self.C]
         
         loc_loss = 0
         obj_loss = 0
+        noobj_loss = 0
 
         for i in range(len(ious1)):
             if ious1[i] > ious2[i]:
-                loc_loss += mse_loss(pred_bbox1[i, :2], target_bbox1[i, :2]) + mse_loss(pred_bbox1[i, 2:4], target_bbox1[i, 2:4])
-                obj_loss += torch.pow((pred_obj1[i] - target_obj1[i]), 2) + self.l_noobj * torch.pow((pred_obj2[i] - 0.0), 2)
+                loc_loss += mse_loss(pred_bbox1[i, :2], target_bbox1[i, :2]) \
+                        + mse_loss(torch.sqrt(torch.abs(pred_bbox1[i, 2:4]) + 1e-6), torch.sqrt(target_bbox1[i, 2:4]))
+                obj_loss += mse_loss(pred_obj1[i] * ious1[i], target_obj1[i])
             else:
-                loc_loss += mse_loss(pred_bbox2[i, :2], target_bbox2[i, :2]) + mse_loss(pred_bbox2[i, 2:4], target_bbox2[i, 2:4])
-                obj_loss += torch.pow((pred_obj2[i] - target_obj2[i]), 2) + self.l_noobj * torch.pow((pred_obj1[i] - 0.0), 2)
+                loc_loss += mse_loss(pred_bbox2[i, :2], target_bbox2[i, :2]) \
+                        + mse_loss(torch.sqrt(torch.abs(pred_bbox2[i, 2:4]) + 1e-6), torch.sqrt(target_bbox2[i, 2:4]))
+                obj_loss += mse_loss(pred_obj2[i] * ious2[i], target_obj2[i])
         
-        # classification loss
-        cls_loss = mse_loss(pred_class, target_class)
+        # noobject loss
+        noobj_mask = target[:, :, :, 4] == 0
+        noobj_pred = pred[noobj_mask]
+        noobj_target = target[noobj_mask]
 
-        return self.l_coord * loc_loss, obj_loss, cls_loss
+        noobj_loss += self.l_noobj * (mse_loss(noobj_pred[:, 4] * ious1, noobj_target[:, 4]) \
+                                    + mse_loss(noobj_pred[:, 9] * ious2, noobj_target[:, 9]))
+
+        # classification loss
+        pred_class = F.softmax(pred_class, dim=1)
+        target_class = torch.argmax(target_class, dim=1)
         
-def calc_iou(boxes1, boxes2):
+        cls_loss = ce_loss(pred_class, target_class)
+
+        return self.l_coord * loc_loss, cls_loss, obj_loss, noobj_loss
+        
+def compute_iou(boxes1, boxes2):
     ious = []
 
     img_size = 448
     divisor = img_size // 7
 
     for box1, box2 in zip(boxes1, boxes2):
-
         x1, y1, w1, h1 = box1
         x2, y2, w2, h2 = box2
 
@@ -81,10 +95,10 @@ def calc_iou(boxes1, boxes2):
         y1 = y1 * divisor
         y2 = y2 * divisor
 
-        w1 = w1 * img_size
-        h1 = h1 * img_size
-        w2 = w2 * img_size
-        h2 = h2 * img_size
+        w1 = w1 ** 2
+        h1 = h1 ** 2
+        w2 = w2 ** 2
+        h2 = h2 ** 2
 
         # Calculate the coordinates of the intersection rectangle
         x_left = max(x1 - w1 / 2, x2 - w2 / 2)
