2caeb34a7f4fa9c9082e9e2a86c6bc71f9ac5335
josh <josh3255@ziovsion.co.kr>
2023-04-25 18:39:13 +0900

    use single prediction

--- utils/loss.py
+++ utils/loss.py
@@ -16,120 +16,74 @@ class YOLOLoss(nn.Module):
 
         self.l_coord = args.l_coord
         self.l_noobj = args.l_noobj
+
+        self.mse_loss = torch.nn.MSELoss()
+        self.ce_loss = torch.nn.CrossEntropyLoss()
         
     def forward(self, pred, target):
+        # Due to the bias that occurs during the learning process, do not use reponsible.
+
         batch_size = pred.shape[0]
 
-        mse_loss = torch.nn.MSELoss()
-        ce_loss = torch.nn.CrossEntropyLoss()
-
         loc_loss = 0
+        obj_loss = 0
+        cls_loss = 0
+        noobj_loss = 0
 
-        # localization loss
         obj_mask = target[:, :, :, 4] > 0
         obj_pred = pred[obj_mask]
         obj_target = target[obj_mask]
-
+        
         pred_bbox1 = obj_pred[:, 0:4]
         pred_bbox2 = obj_pred[:, 5:9]
 
-        target_bbox1 = obj_target[:, 0:4]
-        target_bbox2 = obj_target[:, 5:9]
-        
-        # Use randomly selected boxes for regression 
-        # ious1 = compute_iou(pred_bbox1, target_bbox1)
-        # ious2 = compute_iou(pred_bbox2, target_bbox2)
-        
-        # cx, cy to tx, ty after compute iou
-        # int_tb1 = torch.floor(target_bbox1[:, :2])
-        # target_bbox1[:, :2] = target_bbox1[:, :2] - int_tb1
-        # int_tb2 = torch.floor(target_bbox2[:, :2])
-        # target_bbox2[:, :2] = target_bbox2[:, :2] - int_tb2
-        
-        # object loss
         pred_obj1 = obj_pred[:, 4]
         pred_obj2 = obj_pred[:, 9]
+
         pred_class = obj_pred[:, 5 * self.B : 5 * self.B + self.C]
 
-        target_obj1 = obj_target[:, 4]
-        target_obj2 = obj_target[:, 9]
+        target_bbox = obj_target[:, 0:4]
+        target_obj = obj_target[:, 4]
         target_class = obj_target[:, 5 * self.B : 5 * self.B + self.C]
         
-        loc_loss = 0
-        obj_loss = 0
-        noobj_loss = 0
+        # (obj) localization loss & objectness loss
+        # Use random selected boxes for regression
+        if random.random() > 0.5:
+            loc_loss += self.mse_loss(pred_bbox1, target_bbox)
+            obj_loss += self.mse_loss(pred_obj1, target_obj)
+        else:
+            loc_loss += self.mse_loss(pred_bbox2, target_bbox)
+            obj_loss += self.mse_loss(pred_obj2, target_obj)
         
-        for i in range(len(pred_bbox1)):
-            if random.random() > 0.5:
-                loc_loss += mse_loss(pred_bbox1[i, :2], target_bbox1[i, :2]) \
-                        + mse_loss(pred_bbox1[i, 2:4], torch.sqrt(target_bbox1[i, 2:4]))
-                obj_loss += mse_loss(pred_obj1[i], target_obj1[i])
-            else:
-                loc_loss += mse_loss(pred_bbox2[i, :2], target_bbox2[i, :2]) \
-                        + mse_loss(pred_bbox2[i, 2:4], torch.sqrt(target_bbox2[i, 2:4]))
-                obj_loss += mse_loss(pred_obj2[i], target_obj2[i]) 
+        # (obj) classification loss
+        cls_loss += self.mse_loss(pred_class, target_class)
         
-        # noobject loc loss
+        
         noobj_mask = target[:, :, :, 4] == 0
         noobj_pred = pred[noobj_mask]
         noobj_target = target[noobj_mask]
 
-        noobj_loss = self.l_noobj * (mse_loss(noobj_pred[:, 4], noobj_target[:, 4]) \
-                                    + mse_loss(noobj_pred[:, 9], noobj_target[:, 9]))
+        noobj_pred_bbox1 = noobj_pred[:, 0:4]
+        noobj_pred_bbox2 = noobj_pred[:, 5:9]
 
-        # classification loss
-        pred_class = F.softmax(pred_class, dim=1)
-        target_class = torch.argmax(target_class, dim=1)
-        
-        cls_loss = ce_loss(pred_class, target_class)
+        noobj_pred_obj1 = noobj_pred[:, 4]
+        noobj_pred_obj2 = noobj_pred[:, 9]
 
-        return self.l_coord * loc_loss, cls_loss, obj_loss, noobj_loss
-        
-def compute_iou(boxes1, boxes2):
-    ious = []
+        noobj_pred_class = noobj_pred[:, 5 * self.B : 5 * self.B + self.C]
 
-    img_size = 448
-    divisor = img_size // 7
+        noobj_target_bbox = noobj_target[:, :4]
+        noobj_target_obj = noobj_target[:, 4]
+        noobj_target_class = noobj_target[:, 5 * self.B : 5 * self.B + self.C]
 
-    for box1, box2 in zip(boxes1, boxes2):
-        x1, y1, w1, h1 = box1
-        x2, y2, w2, h2 = box2
+        # (noobj) localization loss
+        noobj_loss += self.mse_loss(noobj_pred_bbox1, noobj_target_bbox)
+        noobj_loss += self.mse_loss(noobj_pred_bbox2, noobj_target_bbox)
 
-        x1 = x1 * divisor
-        x2 = x2 * divisor
-        y1 = y1 * divisor
-        y2 = y2 * divisor
+        # (noobj) objectness loss
+        noobj_loss += self.mse_loss(noobj_pred_obj1, noobj_target_obj)
+        noobj_loss += self.mse_loss(noobj_pred_obj2, noobj_target_obj)
 
-        w1 = w1 ** 2
-        h1 = h1 ** 2
-        w2 = w2 ** 2
-        h2 = h2 ** 2
+        # (noobj) classification loss
+        noobj_loss += self.mse_loss(noobj_pred_class, noobj_target_class)
 
-        # Calculate the coordinates of the intersection rectangle
-        x_left = max(x1 - w1 / 2, x2 - w2 / 2)
-        y_top = max(y1 - h1 / 2, y2 - h2 / 2)
-        x_right = min(x1 + w1 / 2, x2 + w2 / 2)
-        y_bottom = min(y1 + h1 / 2, y2 + h2 / 2)
-        
-        # If the intersection is empty, return 0
-        if x_right < x_left or y_bottom < y_top:
-            iou = torch.tensor(0.0)
-            if torch.cuda.is_available():
-                iou = iou.cuda()
-            ious.append(iou)
-            continue
-        
-        # Calculate the area of intersection rectangle
-        intersection_area = (x_right - x_left) * (y_bottom - y_top)
-        
-        # Calculate the area of both bounding boxes
-        box1_area = w1 * h1
-        box2_area = w2 * h2
-        
-        # Calculate the IOU
-        iou = intersection_area / float(box1_area + box2_area - intersection_area)
-        if torch.cuda.is_available():
-            iou = iou.cuda()
-        
-        ious.append(iou)
-    return ious
+        return self.l_coord * loc_loss / batch_size, cls_loss / batch_size, obj_loss / batch_size, self.l_noobj * noobj_loss / batch_size
